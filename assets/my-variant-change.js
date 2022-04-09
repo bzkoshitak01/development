0c795f327ee0c5c0d58c600b2751eb2d72a3a95a
Daichi Uraie <>
2022-04-10 02:25:08 +0900

    test

--- assets/my-variant-change.js
+++ assets/my-variant-change.js
@@ -1,9 +1,9 @@
-
+console.log('this is my own judgement');
+var selectCallback = function(variant, selector) {
+    console.log('this is test');
+}
 new Shopify.OptionSelectors('product-select', {
     product: {{ product | json }},
     onVariantSelected: selectCallback,
     enableHistoryState: true
 });
-var selectCallback = function(variant, selector) {
-    console.log('this is test');
-}
